a6f58be850f57a67454630ec4a15c5024922e2db
Laur Ferris <lnferris@alum.mit.edu>
2025-04-05 18:37:20 -0400

    test

--- src/CMEMS.jl
+++ src/CMEMS.jl
@@ -1,93 +1,76 @@
-Base.getindex(ds::AbstractDataset,name::Symbol) = DatasetVariable(ds,name)
+Base.getindex(ds::AbstractDataset, name::Symbol) = DatasetVariable(ds, name)
 
-load(dv::DatasetVariable; kwargs...) = load(dv.ds,dv.name; kwargs...)
-download(dv::DatasetVariable; kwargs...) = download(dv.ds,dv.name; kwargs...)
-
+load(dv::DatasetVariable; kwargs...) = load(dv.ds, dv.name; kwargs...)
+download(dv::DatasetVariable; kwargs...) = download(dv.ds, dv.name; kwargs...)
 
 function copernicus_marine_resolve(
-    product_id,dataset_id;
+    product_id, dataset_id;
     asset_name = "timeChunked",
     catalog_url = "https://stac.marine.copernicus.eu/metadata/catalog.stac.json")
-
-    cat = STAC.Catalog(catalog_url);
+    # This function is used to resolve remote URLs.
+    cat = STAC.Catalog(catalog_url)
     list_item = collect(keys(cat[product_id].items))
-    item_canditates = sort(filter(startswith(dataset_id),list_item))
-    # use last version per default
-    dataset_version_id = item_canditates[end]
+    item_candidates = sort(filter(startswith(dataset_id), list_item))
+    # Use the last version by default
+    dataset_version_id = item_candidates[end]
     item = cat[product_id].items[dataset_version_id]
     return STAC.href(item.assets[asset_name])
 end
 
-
 """
-    ds = ROMS.CMEMS_zarr(product_id,mapping,cachedir;
+    ds = ROMS.CMEMS_zarr(product_id, mapping, cachedir;
+                         chunks = 60,
+                         time_shift = 0,
+                         source = :local,
+                         kwargs...)
+
+Returns a structure `ds` that connects to a CMEMS dataset.
+
+`mapping` is a dictionary linking CF standard variable names to the underlying identifiers.
+If a mapping entry is, for example,
+    :sea_surface_height_above_geoid => "so",
+then when `source == :local` the function looks for a file named "so.nc" in `cachedir`.
+If `source == :remote`, the function resolves a URL using `copernicus_marine_resolve`.
+
+Arguments:
+  - `cachedir`: directory where files are cached locally (or a placeholder when using remote URLs)
+  - `source`: either `:local` to use local NetCDF files or `:remote` to use remote URLs.
+  - `chunks`: chunking parameter for the underlying dataset.
+  - `time_shift`: an integer time shift (in seconds) for the dataset.
+"""
+function CMEMS_zarr(product_id, mapping, cachedir;
                     chunks = 60,
                     time_shift = 0,
-                    kwargs...
-                    )
-
-Returns a structure `ds` to connect to a CMEMS zarr server.
-The `mapping` parameter contains a dictorary linking the NetCDF CF standard namer
-to the underlying NetCDF variable names and the product identifers (more
-information is available in the product user manual).
-`cachedir` is a directory where the products are downloaded for caching.
-
-While for most datasets (and CMEMS in the past) the time
-represents the central time the time axis. However since 2024, the time in
-the CMEMS Zarr data represents now the beginning of the time interval.
-Therefore `time_shift` has to be
-added to the time variable to account for this difference.
-For example, if for a daily dataset, the first time instance is the average from
-2000-01-01:00:00:00 to 2000-01-02:00:00:00, then the Zarr file records
-`2000-01-01:00:00:00` (the beginning for the averaging interval) rather than
-`2000-01-01:12:00:00` (the center for the averaging interval). In this case,
-`time_shift` should be `12*60*60` (12 hours in seconds).
-
-
-## Example
-
-The  values of `product_id` and `mapping` (with `dataset_id`)  below are specific to the
-Mediterranean Sea and must be adapted for other domains.
-
-```julia
-outdir = "/tmp"
-product_id = "MEDSEA_MULTIYEAR_PHY_006_004"
-mapping = Dict(
-    # var  dataset_id
-    :sea_surface_height_above_geoid => "med-cmcc-ssh-an-fc-d",
-    :sea_water_potential_temperature => "med-cmcc-tem-an-fc-d",
-    :sea_water_salinity => "med-cmcc-sal-an-fc-d",
-    :eastward_sea_water_velocity => "med-cmcc-cur-an-fc-d",
-    :northward_sea_water_velocity => "med-cmcc-cur-an-fc-d",
-)
-
-dataset = ROMS.CMEMS_zarr(product_id,mapping,outdir,time_shift = 12*60*60)
-```
-
-"""
-function CMEMS_zarr(product_id,mapping,cachedir;
-                    chunks = 60,
-                    time_shift = 0, # time shift in seconds as Int
-                    kwargs...
-                    )
-
-    urls = DefaultDict{Symbol,String,String}("unknown")
-    for (k,v) in mapping
-        dataset_id =
-            if (length(v) > 1) && v isa Tuple
-                v[end]
-            else
-                v
-            end
-        urls[k] = copernicus_marine_resolve(product_id,dataset_id; kwargs...)
+                    source = :local,
+                    kwargs...)
+    files = DefaultDict{Symbol, String, String}("unknown")
+    for (k, v) in mapping
+        dataset_id = (length(v) > 1 && v isa Tuple) ? v[end] : v
+        if source == :local
+            # Build a local file path (e.g. "so.nc") from cachedir
+            files[k] = joinpath(cachedir, string(dataset_id, ".nc"))
+        elseif source == :remote
+            # Resolve the remote URL
+            files[k] = copernicus_marine_resolve(product_id, dataset_id; kwargs...)
+        else
+            error("Unknown source option: $source. Valid options are :local or :remote.")
+        end
     end
 
+    # Choose the dataset type based on source
+    if source == :local
+        dataset_constructor = NCDataset
+    elseif source == :remote
+        dataset_constructor = ZarrDataset
+    else
+        error("Unknown source option: $source. Valid options are :local or :remote.")
+    end
 
     dataset = CDMDataset(
-        ZarrDataset,
-        urls,
+        dataset_constructor,
+        files,
         cachedir = cachedir,
-        options = Dict(:_omitcode => [404,403]),
+        options = Dict(:_omitcode => [404, 403]),
         time_shift = time_shift,
         chunks = chunks)
 
